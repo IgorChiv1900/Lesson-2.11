ec8283342714f613ced76764ed71a00260365640
IgorChiv1900 <chiv1900@mail.ru>
2024-08-20 15:34:42 +0300

    2.11

---
+++ src/Employee.java
@@ -0,0 +1,21 @@
+public class Employee {
+    private int id;
+    private String name;
+    private String surname;
+    private String lastName;
+    private String position;
+    private String email;
+    private long phoneNumbers;
+    private long salary;
+    private int years;
+
+    public Employee (id, name, surname, lastName, position, email, phoneNumbers, salary, years)
+                this.id = id;
+                this.name = name;
+                this.lastName = lastName;
+                this.position = position;
+                this.email = email;
+                this.phoneNumbers = phoneNumbers;
+                this.salary = salary;
+                this.years = years;
+}
---
+++ src/Park.java
@@ -0,0 +1,10 @@
+public class Park {
+    private int id;
+    private String name;
+    private String timeWork;
+
+    public class Attraction;
+    private String nameAttraction;
+    private String openingHours;
+    private int price;
+}
