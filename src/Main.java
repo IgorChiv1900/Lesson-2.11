f40ce4956e2811c2e9e0ae2b2f966ee5dccc8079
IgorChiv1900 <chiv1900@mail.ru>
2024-08-31 07:40:05 +0300

    2.12

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+   public static void (String[] args)
+
+   System.out.println("ФИО: " + this.firstName + " " + this.lastName);
+   System.out.println("Должность: " + this.position);
+   System.out.println("Email: " + this.email);
+   System.out.printf("Телефон: , this.phoneNumber);
+   System.out.printf("Зарплата: , this.salary);
+   System.out.println("Возраст: " + this.age);
+}
