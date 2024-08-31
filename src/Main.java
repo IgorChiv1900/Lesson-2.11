2c0630e1709c54b5aadb3206488c9d3f63ba298a
IgorChiv1900 <chiv1900@mail.ru>
2024-08-31 19:38:28 +0300

    2.12

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
    public static void (String[] args)
 
-   System.out.println("ФИО: " + this.firstName + " " + this.lastName);
-   System.out.println("Должность: " + this.position);
-   System.out.println("Email: " + this.email);
-   System.out.printf("Телефон: , this.phoneNumber);
-   System.out.printf("Зарплата: , this.salary);
-   System.out.println("Возраст: " + this.age);
+   employee.printEmployeeInfo();
 }
